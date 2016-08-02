9ab3bd2cfb7f2c418870abcb9cd6d91a084e2e25
shaojung <shaojung@gmail.com>
2016-08-02 12:01:51 +0800

    完成下載圖片 含進度條

--- app/src/main/java/tw/com/pcschool/t080201/MainActivity.java
+++ app/src/main/java/tw/com/pcschool/t080201/MainActivity.java
@@ -3,11 +3,13 @@ package tw.com.pcschool.t080201;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
@@ -23,11 +25,20 @@ public class MainActivity extends AppCompatActivity {
     Bitmap bitmap;
     Handler handler;
     ImageView img;
+    TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        handler = new Handler();
+        tv = (TextView) findViewById(R.id.textView);
+        handler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+                double s = (double) msg.obj;
+                tv.setText("progress:" + s);
+            }
+        };
         img = (ImageView) findViewById(R.id.imageView);
     }
 
@@ -45,10 +56,15 @@ public class MainActivity extends AppCompatActivity {
                     conn.connect();
                     Log.d("IMG", "conected");
                     InputStream is = conn.getInputStream();
+                    double totalLength = conn.getContentLength();
+                    double sum = 0;
                     int readSize = 0;
                     while ((readSize = is.read(b)) > 0)
                     {
                         bos.write(b, 0, readSize);
+                        sum += readSize;
+                        Message message = handler.obtainMessage(1, sum / totalLength * 100);
+                        handler.sendMessage(message);
                     }
                     byte[] result = bos.toByteArray();
                     Log.d("IMG", "to byte array finished array length:" + result.length);
